4fa8213e01c50f084da5d5c1c798df919fc726d0
Cryothermus <evangrvy@gmail.com>
2020-06-04 21:14:29 -0700

    proper toggle between key types + proper reading

--- src/index.js
+++ src/index.js
@@ -17,20 +17,41 @@ class Decoder extends React.Component {
 
         this.onDecodeChange = this.onDecodeChange.bind(this);
         this.onEncodeChange = this.onEncodeChange.bind(this);
+        this.onKeyBlur = this.onKeyBlur.bind(this);
+        this.onOrderedBlur = this.onOrderedBlur.bind(this);
     }
 
     onEncodeChange(event) {
-        this.setState({encodeContent: event.target.value})
+        this.setState({encodeContent: event.target.value});
     }
 
     onDecodeChange(event) {
-        this.setState({decodeContent: event.target.value})
+        this.setState({decodeContent: event.target.value});
+    }
+
+    onKeyBlur(event) {
+        console.log("Key input read.");
+        this.setState({intKey: parseInt(event.target.value)});
+
+    }
+
+    onOrderedBlur(event) {
+        console.log("Ordered input read.");
+        var inputText = event.target.value;
+        var scannedInts = inputText.match(/\d+/g); //i only vaguely understand how this method works
+        this.setState({orderKey: scannedInts.map(Number)});
+
+
     }
 
     render() {
         return(
             <div>
-                <KeyInput onCheck={(status) => this.setState({isOrdered: status})}></KeyInput>
+                <KeyInput 
+                onCheck={() => this.setState({isOrdered: !(this.state.isOrdered)})}
+                ordered={this.state.isOrdered}
+                keyBlur={this.onKeyBlur}
+                orderedBlur={this.onOrderedBlur}></KeyInput>
                 <Encode onChange={this.onEncodeChange}></Encode>
                 <Decode onChange={this.onDecodeChange}></Decode>
             </div>
--- src/keyInput.js
+++ src/keyInput.js
@@ -2,15 +2,43 @@ import React from 'react';
 //import ReactDOM from 'react-dom';
 
 class KeyInput extends React.Component {
+
+    KeyBox(props) {
+        if (props.ordered) {
+            return (
+                <div>
+                    <label>Ordered Key: </label>
+                    <input 
+                    type="text" 
+                    id="orderedKeyInput" 
+                    onBlur={props.orderedBlur}></input>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div>
+                    <label>Key: </label>
+                    <input 
+                    type="text" 
+                    id="basicKeyInput" 
+                    onBlur={props.keyBlur}
+                    ></input>
+                </div>
+            )
+        }
+    }
+
+
     render() {
         return(
             <div>
-                <label>Basic Key</label>
-                <input type="radio" id="BasicKey" name="keyType" onChange={() => this.props.onCheck(false)} defaultChecked></input>
-                <label>Ordered Key</label>
-                <input type="radio" id="OrderedKey" name="keyType" onChange={() => this.props.onCheck(true)}></input>
-                <label>Key: </label>
-                <input type="text"></input>
+                <label>Use Ordered Key: </label>
+                <input type='checkbox' onClick={() => {this.props.onCheck()}}></input>
+                <this.KeyBox 
+                ordered={this.props.ordered}
+                keyBlur={this.props.keyBlur}
+                orderedBlur={this.props.orderedBlur}></this.KeyBox>
             </div>
         );
     }
